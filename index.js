4fa2ca7fbb61938cc1982a8509fd9447cc8e5aec
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2023-12-18 20:49:43 +0700

    update layout

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const path = require('path'); 
+const bodyPaser = require("body-parser");
 const app = express();
 const Gtts = require('gtts');
+const urlencode = bodyPaser.urlencoded({extended: true});
+app.use(urlencode)
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res, next) => {
+  res.render('home');
+});
 
 app.get('/hear', function (req, res) {
   const gtts = new Gtts(req.query.text, req.query.lang);
@@ -8,5 +18,7 @@ app.get('/hear', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Open url to hear Hallelujah http://localhost:3000/hear?lang=en&text=Hallelujah');
+  console.log('Open url to hear Hallelujah http://localhost:3000/');
 });
+
+// http://localhost:3000/hear?lang=en&text=Hallelujah
